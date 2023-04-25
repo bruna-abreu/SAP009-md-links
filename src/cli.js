debfa64ac12396ce26dddc4013b270119455d60a
bruna-abreu <bruna.abreu95@gmail.com>
2023-04-25 14:25:42 -0300

    processando-diretórios

--- src/cli.js
+++ src/cli.js
@@ -6,14 +6,41 @@ const path = process.argv;
 //console.log(path[2]);
 //getFile(path[2]);
 
-function processText (path) {
-  const result = getFile(path[2])
-    .then((result) => {
-      console.log(chalk.yellow('Lista de links'), result);
-    })
-    .catch((error) => {
-      handleError(error);
-    });
+function linksList(result, identifier ="") {
+  console.log(
+    chalk.yellow('Lista de links'),
+    chalk.black.bgGreen(identifier),
+    result);
+}
+
+function processText(arguments) {
+  const filePath = arguments[2];
+  if (fs.lstatSync(filePath).isFile()) {
+    getFile(filePath)
+      .then((result) => {
+        linksList(result)
+      })
+      .catch((erro) => {
+        console.log(chalk.red('Ocorreu um erro com o arquivo informado. Por favor reveja as informações'))
+      });
+  } else if (fs.lstatSync(filePath).isDirectory()) {
+    fs.promises.readdir(filePath)
+      .then((files) => {
+        files.forEach((fileName => {
+          getFile(`${filePath}/${fileName}`)
+          .then((list) =>{
+            linksList(list, fileName);
+          })
+          .catch((erro) => {
+            console.log(chalk.red('Ocorreu um erro com o diretório informado. Por favor reveja as informações'))
+          })
+        }))
+        //console.log(files);
+      })
+      .catch((erro) => {
+        handleError(erro);
+      });
+  }
 }
 
 processText(path);
